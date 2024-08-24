686f35537b1d8f2857c98cf08823043c1398b2f1
Dragomy <95567734+Dragomy@users.noreply.github.com>
2024-08-24 17:11:52 +0200

    Rework
    Reworked Test Level
    and Movement

--- scr_player.gd
+++ scr_player.gd
@@ -1,13 +1,13 @@
 extends CharacterBody3D
 
 #Envoirenment: Physics
-var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+var gravity = 9.81 * 2
 
 #Movement: Walking
-const BASE_SPEED = 5
+const BASE_SPEED = 8
 var CURRENT_SPEED = BASE_SPEED
-var MAX_SPEED = 20
-const ACCELERATION = 4.0
+const MAX_SPEED = 16.5
+const ACCELERATION = 10
 
 #Movement: jumping
 const JUMP_VELOCITY = 10
@@ -40,8 +40,8 @@ var crouch_on = true
 #Movement: Option Slide
 var slide_on = true
 var isSliding = false
-var slide_speed = CURRENT_SPEED / 2
-var slide_slowdown = 0.0001
+var slide_speed = MAX_SPEED * 2
+var slide_slowdown = 1.01
 
 #Movement: Option Dash
 var dash_on = true
@@ -50,13 +50,17 @@ var is_dashing = false
 #Movement: Option Wallslide
 var wallslide_on = true
 var is_wall_sliding = false
-var WallSlideGravity = gravity / 1.7
+var WallSlideGravity = gravity
 
 #Movement: Option Walljump
 var walljump_on = true
-var WallJumpPushBack = 10
+var WallJumpPushBack = CURRENT_SPEED
+
+@onready var velocity_label = $CanvasLayer/Velocity
+@onready var speed_label = $CanvasLayer/Speed
 
 func _ready():
+	
 	#Capture Mouse Movement
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	# Store the initial camera height
@@ -82,28 +86,30 @@ func _process(_delta):
 	rotate_y(-look_right * controller_sensitivity)
 	camera.rotate_x(-look_up * controller_sensitivity)
 	camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-80), deg_to_rad(87))
-	
+
+
+func _physics_process(delta):
+	velocity_label.text = "Velocity: " + var_to_str(velocity)
+	speed_label.text = "Current Speed: " + var_to_str(CURRENT_SPEED)
+	# Add gravity
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+		
+	walk(delta)
+	jump()
 	run()
 	crouch()
 	slide()
 	groundslam()
 	dash()
-	
-
-func _physics_process(delta):
-	# Add gravity
-	if not is_on_floor():
-		velocity.y -= gravity * delta
-		
-	jump()
-	walk(delta)
-	tiltCamera()
-	wobbleCamera(delta,CURRENT_SPEED)
 	wallslide(delta)
 	walljump()
-	move_and_slide()
+	#CAMERA
+	tiltCamera()
+	wobbleCamera(delta,CURRENT_SPEED)
 	
-#Handle Walking
+	move_and_slide()
+
 func walk(delta):
 	# Get the input direction and handle the movement/deceleration
 	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
@@ -130,46 +136,89 @@ func walk(delta):
 	velocity = velocity.lerp(direction * CURRENT_SPEED, ACCELERATION * delta)
 	velocity.y = y_velocity
 
-func run():
-	if is_on_floor():
-		if Input.is_action_pressed("move_fast") && CURRENT_SPEED < MAX_SPEED:
-			CURRENT_SPEED = CURRENT_SPEED * ACCELERATION
-		elif Input.is_action_just_released("move_fast"):
-			CURRENT_SPEED = BASE_SPEED
-			
-
 func jump():
 	if Input.is_action_just_pressed("move_jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
+var is_running = false
+func run():
+	if is_on_floor():
+		if Input.is_action_pressed("move_fast") && CURRENT_SPEED < MAX_SPEED && !is_running:
+			is_running = true
+			CURRENT_SPEED = MAX_SPEED
+	if Input.is_action_just_released("move_fast"):
+		is_running = false
+		CURRENT_SPEED = BASE_SPEED
+
+#You cant run while Crouched so is_running is set to true this disables running
+#Crouching Halfves the current speed sliding boosts the player fowards for a short duration
 func crouch():
-	if (crouch_on == true):
-		if Input.is_action_pressed("move_crouch") && !isSliding:
+	if crouch_on:
+		if Input.is_action_just_pressed("move_crouch"):
 			scale.y = 0.5
-			CURRENT_SPEED = BASE_SPEED/4
+			CURRENT_SPEED = CURRENT_SPEED / 2
+		elif Input.is_action_pressed("move_crouch"):
+			is_running = true
 		elif Input.is_action_just_released("move_crouch"):
-			scale.y = 1.047
+			scale.y = 1
 			CURRENT_SPEED = BASE_SPEED
+			is_running = false
 
 func slide():
 	if slide_on:
-		if Input.is_action_pressed("move_crouch") and Input.is_action_pressed("move_fast"):
-			if not isSliding:
+		if Input.is_action_pressed("move_fast") && Input.is_action_just_pressed("move_slide"):
+			if !isSliding:
 				isSliding = true
-				velocity *= slide_speed
-			elif isSliding:
-				CURRENT_SPEED *= slide_slowdown
+				CURRENT_SPEED = slide_speed
+		elif Input.is_action_pressed("move_fast") && Input.is_action_pressed("move_slide") && isSliding:
+			CURRENT_SPEED = CURRENT_SPEED / slide_slowdown
 		elif Input.is_action_just_released("move_crouch"):
 			isSliding = false
 			velocity = velocity.normalized() * BASE_SPEED
 
+var is_groundslaming = false
 func groundslam():
 	# If the jump youre to high above ground the animation will play mid air only fix when someone notices :) or tomorrow
 	if !is_on_floor() && bump_on == true:
 		if Input.is_action_just_pressed("move_bump"):
-			velocity.y = -1000
-			slam_animation.play()
+			is_groundslaming = true
+			velocity.y = -100
+	elif is_on_floor() && is_groundslaming:
+		is_groundslaming = false
+		slam_animation.play()
 
+func dash():
+	if (dash_on == true):
+		if Input.is_action_just_pressed("move_dash") && !is_on_floor() && !is_dashing:
+			CURRENT_SPEED *= 4
+			is_dashing = true
+		elif !is_on_floor() && is_dashing:
+			CURRENT_SPEED = CURRENT_SPEED / 1.05
+		elif is_on_floor() && is_dashing: 
+			is_dashing = false
+			CURRENT_SPEED = BASE_SPEED
+
+func wallslide(delta):
+	if walljump_on:
+		if is_on_wall() && !is_on_floor():
+			# Sepperated for Camera animation not jet implemented
+			if Input.is_action_pressed("move_left"):
+				is_wall_sliding = true
+				velocity.y = -0.8
+			elif Input.is_action_pressed("move_right"):
+				is_wall_sliding = true
+				velocity.y = -0.8
+			else:
+				is_wall_sliding = false
+		else:
+			is_wall_sliding = false
+
+func walljump():
+	if walljump_on:
+		if is_wall_sliding and Input.is_action_pressed("move_jump"):
+			velocity.y += JUMP_VELOCITY
+
+# CAMERA 
 func tiltCamera():
 	if tilt_on == true:
 		if Input.is_action_pressed("move_left"):
@@ -191,38 +240,3 @@ func wobbleCamera(delta: float, player_speed: float):
 			camera.position.y = base_camera_height
 			# Reset wobble time
 			wobble_time = 0.0  
-
-func dash():
-	if (dash_on == true):
-		if Input.is_action_just_pressed("move_dash") && !is_on_floor() && !is_dashing:
-			velocity *= 4
-			is_dashing = true
-		elif Input.is_action_just_released("move_dash"):
-			velocity /= 2
-			CURRENT_SPEED = BASE_SPEED
-		elif is_on_floor(): 
-			is_dashing = false
-
-func wallslide(delta):
-	if walljump_on:
-		if is_on_wall() and not is_on_floor():
-			if Input.is_action_pressed("move_left") or Input.is_action_pressed("move_right"):
-				is_wall_sliding = true
-			else:
-				is_wall_sliding = false
-		else:
-			is_wall_sliding = false
-			
-		if is_wall_sliding:
-			velocity.y += (WallSlideGravity * delta)
-			velocity.y = min(velocity.y, WallSlideGravity)
-
-func walljump():
-	if walljump_on:
-		if is_wall_sliding and Input.is_action_pressed("move_jump"):
-			velocity.y += JUMP_VELOCITY
-			if Input.is_action_pressed("move_left"):
-				velocity.x += WallJumpPushBack
-			if Input.is_action_pressed("move_right"):
-				velocity.x += -WallJumpPushBack
-		
